5539516bb75b51b922753817b28e1f60366fddbb
Michael Beirne <133065459+michaelbeirnee@users.noreply.github.com>
2026-06-07 23:46:17 +0000

    updates - super technical

---
+++ src/main/java/com/example/funblog/controller/PublicController.java
@@ -0,0 +1,10 @@
+//tells java this files eblongs inside the controller package 
+package com.example.funblog.controller; 
+
+//imports the article class so this controlelr can work with aritcle object
+import com.example.funblog.model.Article;
+
+//article service so the ccontroller can ask for article data
+import com.exmaple.funblog.service.ArticleService;
+
+//import controller which tells sprig this class handles web pages 
--- src/main/java/com/example/funblog/service/ArticleService.java
+++ src/main/java/com/example/funblog/service/ArticleService.java
@@ -51,4 +51,95 @@ public class ArticleService{
             throw new RuntimeException("Could not create articles foldert", exception); 
         }
     }
+
+    //Method gets every article from the articles folder 
+    public List<Article> getAllArticles(){
+
+        //create an empty array list to hold all articles
+        List<Article> articles = new ArrayList<>();
+
+        //Open the articels folder and only look for files ending with .json
+        try(DirectoryStream<Path> files = Files.newDirectoryStream(articlesFolder, "*.json")){
+            for(Path file : files){
+                
+                Article article = objectMapper.readValue(file.toFile(), Article.class); 
+                articles.add(article); 
+
+            }
+            //Catch file errors that could happen while reading article files
+        }catch(IOException exception){
+            //Stop the app and show a clearer error message
+            throw new RunTimeException("Could not load articles", exception); 
+        }
+
+        return articles; 
+    }
+
+    public Article getArticleById(String id){
+        //Build the file path - like article/ my first post.json
+        Path articleFile = articlesFolder.resolve(id + ".json");
+
+        //If the article file does not exist = return null because no articles were found
+        if(!Files.exists(articleFile)){
+            return null; 
+        }
+
+        try{
+            //Read the Json file and turn it into an Article object 
+            return objectMapper.readValue(articleFile.toFile(), Article.class); //why do we invoke .class 
+        } catch (IOException exception){
+            //Stop the app and show a clearer error message 
+            throw new RuntimeException("Could not load article", exception); 
+        }
+    }
+
+    //Method create a new article file
+    public void createArticle(Article article){
+
+        //convert article title to id 
+        String id = convertTitleToId(article.getTitle()); 
+        //Store the generated id inside the article object 
+        article.setId(id); 
+        //Build the file path where the article will be saved
+        Path articleFile = articlesFolder.resolve(id + ".json"); 
+
+        try{
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(articleFile.toFile(), article); 
+        }catch (IOException exception){
+            throw new RuntimeException("could not save article", exception); 
+        }
+    }
+
+    public void updateArticle(String id, Article article){
+
+        //keep the id the same 
+        article.setId(id); 
+
+        //build the path to the exisiting article file
+        //resolve == .add()
+        Path articleFile = articlesFolder.resolve(id + ".json"); 
+
+        //converting out Article article "object" into json AKA website posting 
+        try{
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(articleFile.toFile(), article); 
+        }catch (IOException exception){
+            throw new RuntimeException("Could not update Article", exception); 
+        }
+    }
+    
+    public void deleteArticle(String id){
+
+        Path articleFile = articlesFolder.resolve(id + ".json"); 
+        try{
+            Files.deleteIfExists(articleFile); 
+        }catch (IOException exception){
+            throw new RuntimeException("couldn't find it ", exception); 
+        }
+    }
+
+
+    private String convertTitleToId(String title){
+        //Start with the title
+        return title.toLowerCase().replaceAll("[^a-z0-9 ]", "").trim().replaceAll("\\s+", "-"); 
+    }
 }
